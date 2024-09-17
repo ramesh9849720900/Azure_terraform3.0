0a4d1c23c5666d804e7b0013ea2deb8ce2aae418
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2024-09-17 15:45:57 +0530

    Add files via upload

---
+++ create-app-svc.tf
@@ -0,0 +1,36 @@
+# Create a resource group
+resource "azurerm_resource_group" "dev-rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Create app service plan
+resource "azurerm_app_service_plan" "service-plan" {
+  name = "simple-service-plan"
+  location = azurerm_resource_group.dev-rg.location
+  resource_group_name = azurerm_resource_group.dev-rg.name
+  kind = "Linux"
+  reserved = true
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Create JAVA app service
+resource "azurerm_app_service" "app-service" {
+  name = "hexaware-app-service"
+  location = azurerm_resource_group.dev-rg.location
+  resource_group_name = azurerm_resource_group.dev-rg.name
+  app_service_plan_id = azurerm_app_service_plan.service-plan.id
+
+site_config {
+    linux_fx_version = "TOMCAT|8.5-java11"
+  }
+tags = {
+    environment = "dev"
+  }
+}
